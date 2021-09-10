62c18e759985a2ae9e44931dd339d12f6558ec0c
Mattias Högnäs <mattias.hognas@spp.se>
2021-09-10 20:58:24 +0200

    Adding specflow

---
+++ lab.api/Controllers/Calculator.cs
@@ -0,0 +1,10 @@
+namespace lab.api.Controllers
+{
+    public class Calculator
+    {
+        public int Add(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+    }
+}
